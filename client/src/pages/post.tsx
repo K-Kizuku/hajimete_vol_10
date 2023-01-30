05f60bbd967e51954e6f11f60218469a589cd90d
K-Kizuku <kotani.kizuku@gmail.com>
2023-01-30 13:27:03 +0900

    [add] 投稿フォームのCSSの追加

--- client/src/pages/post.tsx
+++ client/src/pages/post.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
+
 const Post = () => {
+    const [isCheck, setIsCheck] = useState(true)
     return(
         <div className="bg-blue-200 h-screen">
             <div className="p-8">
-            <label>test</label><input />
-            <button>投稿</button>
+            <label>タイトル</label><input className="px-4 py-2 m-5"/>
+            <button className="bg-gradient-to-br from-blue-300 to-blue-800 hover:bg-gradient-to-tl text-white rounded px-4 py-2">投稿</button>
             </div>
-                
+            <label>
+                <>
+                    <input
+                    type="checkbox"
+                    className="peer sr-only"
+                    checked={isCheck}
+                    onClick={()=>setIsCheck(!isCheck)}
+                    />
+                    <span
+                    className="block w-[2em] cursor-pointer bg-gray-500 rounded-full 
+                    p-[1px] after:block after:h-[1em] after:w-[1em] after:rounded-full 
+                    after:bg-white after:transition peer-checked:bg-blue-500 
+                    peer-checked:after:translate-x-[calc(100%-2px)]"
+                    >
+                    </span>
+                    { isCheck ? 'public' : 'private' }
+                </>
+            </label>
         </div>
     )
 }
